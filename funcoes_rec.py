b0170e3e013c4a1fec8c1db41b5259719be01ccd
lucas alkaid <alkaidw@gmail.com>
2020-09-07 23:46:56 -0300

    Add files via upload

---
+++ funcoes_rec.py
@@ -0,0 +1,41 @@
+def incr(a):
+    return a + 1
+
+def decr(a): 
+    return _decr(a, 0)
+
+def _decr(a, i):
+    if a > incr(i):
+        return _decr(a, incr(i))
+    return i
+    
+def adic(a, b):
+    if b == 0:
+        return a
+    return incr(adic(a,decr(b)))
+    
+def subtr(a, b):
+    if b > 0:
+        return subtr(decr(a), decr(b))
+    return a
+
+def mult(a, b):
+    if b == 0:
+        return 0
+    return adic(a, mult(a, decr(b)))
+
+def div(n, d):
+    if n > d or n == d:
+        return incr(div(subtr(n, d), d))
+    return 0
+
+def mod(n, d):
+    if n > d or n == d:
+        return mod(subtr(n, d), d)
+    return n
+ 
+def exp(a, b):
+    if b == 0:
+        return 0
+    return mult(a, mult(a, decr(b)))
+
